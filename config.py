cc303b8208effc4cece57d69ab7880a9c10db95b
meghanshimathur <115732333+meghanshimathur@users.noreply.github.com>
2026-03-30 17:42:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,36 @@
+
+
+
+# DISASTER DATA SOURCES
+DATA_SOURCES = [
+    {
+        "name": "USGS",
+        "url": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
+        "type": "usgs"
+    },
+    {
+        "name": "NewsAPI",
+        "url": "https://newsapi.org/v2/everything",
+        "type": "newsapi"
+    },
+    {
+        "name": "NewsData",
+        "url": "https://newsdata.io/api/1/news",
+        "type": "newsdata"
+    },
+     {
+        "name": "NDMA",
+        "url": "https://sachet.ndma.gov.in/",
+        "type": "static"
+    },
+    {
+        "name": "NASA",
+        "url": "https://disasters.nasa.gov/",
+        "type": "static"
+    },
+    {
+        "name": "FEMA",
+        "url": "https://www.fema.gov/",
+        "type": "static"
+    }
+]
